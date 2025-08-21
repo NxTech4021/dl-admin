8780770c7bf063ec039387dc0379be15c5d0ebf8
Willy Wong <willywong@x1s-Macbook-Air.local>
2025-08-21 15:23:11 +0800

    feat: site header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,8 +1,20 @@
+"use client"
+
+import { usePathname } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
 
+const pageMap = {
+  "/dashboard": "Dashboard",
+  "/players": "Players",
+  "/settings": "Settings",
+} as const
+
 export function SiteHeader() {
+  const pathname = usePathname()
+  const pageTitle = pageMap[pathname as keyof typeof pageMap] || "Dashboard"
+
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -11,7 +23,7 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">Documents</h1>
+        <h1 className="text-base font-medium">{pageTitle}</h1>
         <div className="ml-auto flex items-center gap-2">
         </div>
       </div>
