69fecf9b46f6cec10c1fd647595c1b6765de6f0a
nekkaida <kennethriadinugroho@gmail.com>
2025-11-28 12:32:06 +0800

    style: format aspect-ratio component

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -1,11 +1,12 @@
 "use client"
 
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
+// OPTIMIZATION: Named imports instead of wildcard
+import { Root as AspectRatioRoot } from "@radix-ui/react-aspect-ratio"
 
 function AspectRatio({
   ...props
-}: React.ComponentProps<typeof AspectRatioPrimitive.Root>) {
-  return <AspectRatioPrimitive.Root data-slot="aspect-ratio" {...props} />
+}: React.ComponentProps<typeof AspectRatioRoot>) {
+  return <AspectRatioRoot data-slot="aspect-ratio" {...props} />
 }
 
 export { AspectRatio }
